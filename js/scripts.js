6a6ebb4cb5be77e19546cd52da5aeac497ddd5d6
Brittany Gollins <brittany.gollins@gmail.com>
2018-10-04 22:45:52 -0700

    if else statements now functioning to respond with pingpong, pong, or ping, if the user input is divisible by 15, 5, or 3, respectively. Results, the number array holding the number responses, show on the page after user submits input. Need to format into unordered list still.

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,26 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-    debugger
     var number = parseInt($("input#number").val());
     var numberAbs = Math.abs(number);
     var numberArray = []
     for (var index = 1; index <= numberAbs; index += 1) {
-      numberArray.push(index);
+  //    numberArray.push(index);
+      if (index % 15 == 0) {
+        numberArray.push("pingpong")
+//       $(".result").text("ping")
+      } else if (index % 5 == 0) {
+        numberArray.push("pong")
+//       $(".result").text("pong")
+      } else if (index % 3 == 0) {
+//       $(".result").text("pingpong")
+        numberArray.push("ping")
+      } else {
+//      $(".result").text(index)
+        numberArray.push(index)
+      }
     }
-
-    if (numberAbs % 3 == 0) {
-      $(".result").text("ping")
-    } else if (numberAbs % 5 == 0) {
-      $(".result").text("pong")
-    } else if (numberAbs % 15 == 0){
-      $(".result").text("pingpong")
-    } else {
-      $(".result").text(numberAbs)
-    }
-
+    $(".result").text(numberArray);
     $("#result").show();
   });
 });
