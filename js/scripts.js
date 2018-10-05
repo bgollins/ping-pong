a3bcfa1d09aa1d3906e7e7e603e6a05b666ab91d
Brittany Gollins <brittany.gollins@gmail.com>
2018-10-04 22:27:58 -0700

    take user input and turn into its absolute value. create a new array. use for loop to push series of numbers counting up from 1 to the user input number into an array. existing if else statements are nonfunctioning.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,24 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
+    debugger
     var number = parseInt($("input#number").val());
-
-    if (number % 3) {
-      $(".result").text("ping")
-    } else if (number % 5) {
-      $(".result").text("pong")
-    } else {
-      $(".result").text("pingpong")
+    var numberAbs = Math.abs(number);
+    var numberArray = []
+    for (var index = 1; index <= numberAbs; index += 1) {
+      numberArray.push(index);
     }
-    
+
+    if (numberAbs % 3 == 0) {
+      $(".result").text("ping")
+    } else if (numberAbs % 5 == 0) {
+      $(".result").text("pong")
+    } else if (numberAbs % 15 == 0){
+      $(".result").text("pingpong")
+    } else {
+      $(".result").text(numberAbs)
+    }
+
     $("#result").show();
   });
 });
