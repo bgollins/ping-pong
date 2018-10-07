f79622c2a6451d196a77fb35f062c1da82fdeace
Brittany Gollins <brittany.gollins@gmail.com>
2018-10-07 16:39:22 -0700

    add column formatting with background colors. Can't quite get the setup right.

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,18 @@ $(document).ready(function() {
     var numberAbs = Math.abs(number);
     var numberArray = []
     for (var index = 1; index <= numberAbs; index += 1) {
-  //    numberArray.push(index);
+
       if (index % 15 == 0) {
         numberArray.push("pingpong")
-//       $(".result").text("ping")
       } else if (index % 5 == 0) {
         numberArray.push("pong")
-//       $(".result").text("pong")
       } else if (index % 3 == 0) {
-//       $(".result").text("pingpong")
         numberArray.push("ping")
       } else {
-//      $(".result").text(index)
         numberArray.push(index)
       }
     }
+
     $(".result").text(numberArray);
     $("#result").show();
   });
