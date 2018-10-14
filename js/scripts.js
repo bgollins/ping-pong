eead6fe2454fa073370fd81fe7834fc527df109e
Brittany Gollins <brittany.gollins@gmail.com>
2018-10-14 15:00:27 -0700

    update scripts.js to separate biz logic from UI logic and make updates to HTML and scripts.js for clearer reading

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
+//Biz logic
+function pongMachine(numberAbs) {
+  var numberArray = []
+  for (var index = 1; index <= numberAbs; index += 1) {
+
+    if (index % 15 == 0) {
+      numberArray.push("pingpong")
+    } else if (index % 5 == 0) {
+      numberArray.push("pong")
+    } else if (index % 3 == 0) {
+      numberArray.push("ping")
+    } else {
+      numberArray.push(index)
+    }
+  }
+  return numberArray;
+};
+
+//UI logic
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
     var numberAbs = Math.abs(number);
-    var numberArray = []
-    for (var index = 1; index <= numberAbs; index += 1) {
-
-      if (index % 15 == 0) {
-        numberArray.push("pingpong")
-      } else if (index % 5 == 0) {
-        numberArray.push("pong")
-      } else if (index % 3 == 0) {
-        numberArray.push("ping")
-      } else {
-        numberArray.push(index)
-      }
-    }
-
-    $(".result").text(numberArray);
-    $("#result").show();
+    var numberArray = pongMachine(numberAbs);
+    numberArray.forEach(function(x) {
+      $('#resultList').append("<li>" + x + "</li>");
+    });
+    //$(".result").text(numberArray);
+    $("#result").show(numberArray);
   });
 });
